c79c37c0a3e0866c9919b79d997695db40342952
Khadidja <khadija.aitsiali@gmail.com>
2022-07-04 01:29:47 +0200

    menu: working on tablet size screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from './../assets/illustrations/icon-logo.png'
 import './../styles/header.css';
 
 // Icons
-import { IconHome, IconLicense, IconBriefcase, IconSend } from '@tabler/icons';
+import { IconHome, IconLicense, IconBriefcase, IconSend, IconBrandLinkedin, IconBrandGmail, IconBrandGithub } from '@tabler/icons';
 
 const Header = () => {
   return (
@@ -13,32 +13,46 @@ const Header = () => {
       {/* Menu for tablet/desktop */}
       <nav className='navbar'>
 
-        <NavLink className="navbar-logo" to="/">
-          <img src={logo} alt="Logo Khadidja A." />
-        </NavLink>
-
         <ul className='navbar-links'>
           <li className='navbar-item'>
+            <NavLink to="/">
+              <img src={logo} alt="Logo Khadidja A." />
+            </NavLink>
+          </li>
+          <li className='navbar-item'>
             <NavLink className='navbar-link' to="/">
-              <span className="link-number">01</span> home
+              <IconHome size={30} stroke={1.5}/> <br /> 
+              <span className='navbar-subtitle' >home</span>
             </NavLink>
           </li>
           <li className='navbar-item'>
             <NavLink className='navbar-link' to="/services">
-            <span className="link-number">02</span> services
+              <IconLicense size={30} stroke={1.5}/> <br /> 
+              <span className='navbar-subtitle' >serv <b></b> ices</span>
             </NavLink>
           </li>
           <li className='navbar-item'>
             <NavLink className='navbar-link' to="/portfolio">
-            <span className="link-number">03</span> portfolio
+              <IconBriefcase size={30} stroke={1.5}/> <br /> 
+              <span className='navbar-subtitle' >port <br /> folio</span>
             </NavLink>
           </li>
           <li className='navbar-item'>
             <NavLink className='navbar-link' to="/contact">
-            <span className="link-number">04</span> contact
+              <IconSend size={30} stroke={1.5}/> <br /> 
+              <span className='navbar-subtitle' >contact</span>
             </NavLink>
           </li>
         </ul>
+
+        <div className="social-media">
+          <h1>contact</h1>
+          <p class="social-banner">
+            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><IconBrandLinkedin size={30} stroke={1.5}/></a>
+            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><IconBrandGmail size={30} stroke={1.5}/></a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><IconBrandGithub size={30} stroke={1.5}/></a>
+          </p>
+        </div>
 
       </nav>
 
